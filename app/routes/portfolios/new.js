c355997b17ea20bc020b105fa0422521f99ce2a0
Blake Gentry <blakesgentry@gmail.com>
2015-01-08 13:12:46 -0800

    use ember-data-route to rollback unsaved models
    Without it, models added to the store with createRecord i.e. on the /new
    page are left in the store if the user navigates away to the index page
    before saving the model.

--- app/routes/portfolios/new.js
+++ app/routes/portfolios/new.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import DataRoute from 'ember-data-route/mixins/data-route';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(DataRoute, {
   model: function(params) {
     return this.store.createRecord('portfolio', params);
   },
