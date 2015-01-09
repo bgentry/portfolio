1b0eed217392b6df2a8e18392d66c5ac9bafb9f1
Blake Gentry <blakesgentry@gmail.com>
2015-01-08 20:45:23 -0800

    add asset class as soon as selection is made

--- app/controllers/portfolios/new.js
+++ app/controllers/portfolios/new.js
@@ -2,20 +2,21 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   assetClassToAdd: null,
+  onAssetClassToAddChange:function(){
+    var assetClass = this.get('assetClassToAdd');
+    if(assetClass === null) {
+      return;
+    }
+    var portfolio = this.get('model');
+    var allocation = this.store.createRecord('allocation', {
+      assetClass: assetClass,
+      portfolio: portfolio,
+    });
+    portfolio.get('allocations').addObject(allocation);
+    this.set('assetClassToAdd', null);
+  }.observes('assetClassToAdd'),
+
   actions: {
-    addAllocation: function() {
-      var assetClass = this.get('assetClassToAdd');
-      if(assetClass === null) {
-        return;
-      }
-      var portfolio = this.get('model');
-      var allocation = this.store.createRecord('allocation', {
-        assetClass: assetClass,
-        portfolio: portfolio,
-      });
-      portfolio.get('allocations').addObject(allocation);
-      this.set('assetClassToAdd', null);
-    },
     create: function(params) {
       console.log("CREATE PARAMS: ", params);
       var newPortfolio = this.get('model');
