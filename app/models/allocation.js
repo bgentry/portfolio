e95cf86697390fa259ff445e18be9691275ec36c
Blake Gentry <blakesgentry@gmail.com>
2015-01-11 20:26:49 -0800

    ignore divshot files

--- app/models/allocation.js
+++ app/models/allocation.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  assetClass: DS.belongsTo('asset-class', { async: true }),
+  // TODO: assetClass should be async: true, but there's a bug w/ that atm
+  assetClass: DS.belongsTo('asset-class'),
   portfolio: DS.belongsTo('portfolio'),
 
   weight: DS.attr('percentage', {
