64a4c6d0db1d311a8a954725104edafba7124f62
Blake Gentry <blakesgentry@gmail.com>
2015-01-16 15:10:00 -0800

    alias for allocation assetClass name

--- app/models/allocation.js
+++ app/models/allocation.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   assetClass: DS.belongsTo('asset-class', { async: true }),
   portfolio: DS.belongsTo('portfolio'),
-
   weight: DS.attr('percentage', {
     defaultValue: function() { return 0; }
-  })
+  }),
+
+  assetClassName: Ember.computed.alias('assetClass.name')
 });
