c18a01bd3a78034510ed91916cf298f8092f6e46
Blake Gentry <blakesgentry@gmail.com>
2015-01-17 14:30:45 -0800

    show currency symbol in format-currency

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -5,7 +5,7 @@ export function formatCurrency(value) {
   if (value === undefined || value === null) {
     return null;
   }
-  return currency(value).format();
+  return currency(value).format(true);
 }
 
 export default Ember.Handlebars.makeBoundHelper(formatCurrency);
