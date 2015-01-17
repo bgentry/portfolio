63be32d617ac73fb7e81138fbf802179c24a3c12
Blake Gentry <blakesgentry@gmail.com>
2015-01-16 18:17:52 -0800

    better perc rounding

--- app/helpers/format-percentage.js
+++ app/helpers/format-percentage.js
@@ -1,13 +1,28 @@
 import Ember from 'ember';
 
+// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+function decimalAdjust(type, value, exp) {
+  // If the exp is undefined or zero...
+  if (typeof exp === 'undefined' || +exp === 0) {
+      return Math[type](value);
+  }
+  value = +value;
+  exp = +exp;
+  // If the value is not a number or the exp is not an integer...
+  if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+      return NaN;
+  }
+  // Shift
+  value = value.toString().split('e');
+  value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+  // Shift back
+  value = value.toString().split('e');
+  return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+}
+
 export function formatPercentage(percentage) {
   percentage = percentage * 100.0;
-  var wholePercs = Math.floor(percentage),
-      decimalPortion = Math.abs(percentage - wholePercs);
-  if (decimalPortion < 0.1) {
-    return wholePercs.toString()+'%';
-  }
-  return percentage.toFixed(1)+'%';
+  return decimalAdjust('round', percentage, -1) + '%';
 }
 
 export default Ember.Handlebars.makeBoundHelper(formatPercentage);
--- tests/unit/helpers/format-percentage-test.js
+++ tests/unit/helpers/format-percentage-test.js
@@ -5,9 +5,10 @@ import {
 module('FormatPercentageHelper');
 
 test('it formats percentages correctly', function() {
-  equal(formatPercentage(.42), '42%', "42 is converted into 42%");
-  equal(formatPercentage(.420), '42%', "42.0 is converted into 42%");
-  equal(formatPercentage(.421), '42.1%', "42.1 is converted into 42.1%");
-  equal(formatPercentage(.4205), '42%', "42.05 is converted into 42%");
-  equal(formatPercentage(.4215), '42.1%', "42.15 is converted into 42.1%");
+  equal(formatPercentage(.42), '42%', ".42 is converted into 42%");
+  equal(formatPercentage(.420), '42%', ".420 is converted into 42%");
+  equal(formatPercentage(.421), '42.1%', ".421 is converted into 42.1%");
+  equal(formatPercentage(.4204), '42%', ".4204 is converted into 42%");
+  equal(formatPercentage(.4215), '42.2%', ".4215 is converted into 42.2%");
+  equal(formatPercentage(.999999), '100%', ".999999 is converted into 42.1%");
 });
