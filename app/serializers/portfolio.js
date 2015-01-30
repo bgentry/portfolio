b289df1a6857229ed188e42a1ae5d20a4475709b
Blake Gentry <blakesgentry@gmail.com>
2015-01-30 10:29:51 -0800

    remove portfolio_id from embedded allocation serialization

--- app/serializers/portfolio.js
+++ app/serializers/portfolio.js
@@ -3,5 +3,13 @@ import DS from 'ember-data';
 export default DS.ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
     allocations: {embedded: 'always'}
+  },
+  serializeHasMany: function(record, json, relationship) {
+    this._super(record, json, relationship);
+    if (relationship.key === "allocations") {
+      json.allocations.forEach(function(hash) {
+        delete hash.portfolio_id;
+      });
+    }
   }
 });
